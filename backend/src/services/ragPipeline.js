6cbdac751d11a18c65685dd385d07855d2200c64
Hawyaa <hawi4936@gmail.com>
2026-05-29 04:26:48 -0700

    Update ragPipeline.js

--- backend/src/services/ragPipeline.js
+++ backend/src/services/ragPipeline.js
@@ -4,7 +4,7 @@ const { generateRAGResponse, isGreeting } = require('./llm');
 
 // Minimum similarity score to consider a result relevant (0-1)
 // const MIN_RELEVANCE_SCORE = 0.72;
-const MIN_RELEVANCE_SCORE = 0.3;
+const MIN_RELEVANCE_SCORE = 0.72;
 
 // Number of top chunks to retrieve
 const TOP_K = 6;
